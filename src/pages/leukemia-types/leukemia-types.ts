9957df7f4581c3667cb9d32942bdd9b0de62433f
movarind <u17045780@tuks.co.za>
2019-05-23 13:26:46 +0200

    All data displayed for all pages

--- src/pages/leukemia-types/leukemia-types.ts
+++ src/pages/leukemia-types/leukemia-types.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { DataProvider } from '../../providers/data/data'
 @Component({
   selector: 'page-leukemia-types',
   templateUrl: 'leukemia-types.html'
 })
 export class LeukemiaTypesPage {
-
-  constructor(public navCtrl: NavController) {
+  lukemias: any;
+  lukemiatypes: any;
+  constructor(public navCtrl: NavController, public dataProvider: DataProvider) {
+    this.getLuemias();
+    this.getLuemiaTypes();
   }
-  
+  getLuemias() {
+    this.dataProvider.getLukemia()
+    .then(data => {
+    this.lukemias = data;
+    console.log(this.lukemias);
+    });
+    }
+  getLuemiaTypes() {
+    this.dataProvider.getLukemiaType()
+    .then(data => {
+    this.lukemiatypes = data;
+    console.log(this.lukemiatypes);
+    });
+    }
 }
--- src/pages/medication/medication.ts
+++ src/pages/medication/medication.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { DataProvider } from '../../providers/data/data';
 @Component({
   selector: 'page-medication',
   templateUrl: 'medication.html'
 })
 export class MedicationPage {
-
-  constructor(public navCtrl: NavController) {
+  medications: any;
+  medicationtypes: any;
+  constructor(public navCtrl: NavController, public dataProvider: DataProvider) {
+    this.getMedications();
+    this.getMedicationTypes();
   }
-  
+  getMedications() {
+    this.dataProvider.getMedication()
+    .then(data => {
+    this.medications = data;
+    console.log(this.medications);
+    });
+    }
+    getMedicationTypes() {
+    this.dataProvider.getMedicationType()
+    .then(data => {
+    this.medicationtypes = data;
+    console.log(this.medicationtypes);
+    });
+    }
 }
